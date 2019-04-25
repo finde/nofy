7164a5ab97b6a05d0b49af95f2fc9b4834b64df1
finde <finde.xumara@gmail.com>
2019-04-25 07:26:02 +0200

    fixed assets path

--- components/Assets.js
+++ components/Assets.js
@@ -3,7 +3,7 @@ const expressFileUpload = require('express-fileupload');
 const path = require('path');
 
 module.exports = function Assets(nofy, { express, config }, cb) {
-  const staticPath = path.resolve(nofy.rootDir, '..', config.static);
+  const staticPath = path.resolve(nofy.rootDir, config.static);
   express.use('/assets', Express.static(staticPath));
   express.use(expressFileUpload()); // allow upload - parser
   return cb(true);
