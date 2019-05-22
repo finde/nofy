0612023742c7d76684ec2ed9552adab2500dc403
finde <finde.xumara@gmail.com>
2019-05-22 16:14:08 +0200

    ignore db

--- components/Database.js
+++ components/Database.js
@@ -2,21 +2,25 @@ const mongoose = require('mongoose');
 const MongodbUriParser = require('mongodb-uri');
 
 module.exports = function Database(nofy, { config }, cb) {
-  const databaseUri = MongodbUriParser.format(config.db);
+  if (config.db) {
+    const databaseUri = MongodbUriParser.format(config.db);
 
-  mongoose.connection.on('error', (err) => {
-    console.log('Mongoose Connection Error', err)
-  });
-  mongoose.connection.on('connected', () =>  cb(true));
-  mongoose.connection.on('reconnected', () => cb(true));
-
-  function connectWithRetry() {
-    return mongoose.connect(databaseUri, { useNewUrlParser: true }, (err) => {
-      if (err) {
-        setTimeout(connectWithRetry, 5000);
-      }
+    mongoose.connection.on('error', (err) => {
+      console.log('Mongoose Connection Error', err)
     });
-  }
+    mongoose.connection.on('connected', () => cb(true));
+    mongoose.connection.on('reconnected', () => cb(true));
 
-  connectWithRetry();
+    function connectWithRetry() {
+      return mongoose.connect(databaseUri, { useNewUrlParser: true }, (err) => {
+        if (err) {
+          setTimeout(connectWithRetry, 5000);
+        }
+      });
+    }
+
+    connectWithRetry();
+  } else {
+    cb(true);
+  }
 };
